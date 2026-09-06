51fa03c792bf18a372fae63354273bc30acde305
Lisa Gehrig <lisa.gehrig2@uzh.ch>
2026-09-07 00:50:51 +0200

    add image

--- frabo.py
+++ frabo.py
@@ -66,7 +66,12 @@ PAGES = [
 
             {"type": "markdown", "label": """## On the day — preparations"""},
             {"type": "markdown", "label": """A psilocybin trip lasts around **4-6 hours.**"""},
-
+            {
+                "label": """ "de muet zum chaos" """,
+                "type": "image",
+                "src": "curve.png",
+                "width": 350,
+            },
             {"type": "markdown", "label": """**Your head:**
 
 - Cultivate a mindset to fully accept and welcome everything that comes.
